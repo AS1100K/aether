af1164cc29b002f0db1ad9cffc7f5b43ed7e815a
Aditya Kumar <theadityakumardeveloper@gmail.com>
2024-05-09 17:25:19 +0530

    Initialized Basic Chat Logging Code for the bot

---
+++ aether-core/src/main.rs
@@ -0,0 +1,29 @@
+use azalea::prelude::*;
+use parking_lot::Mutex;
+use std::sync::Arc;
+
+#[tokio::main]
+async fn main() {
+    let account = Account::offline("_aether");
+    // or Account::microsoft("example@example.com").await.unwrap();
+
+    ClientBuilder::new()
+        .set_handler(handle)
+        .start(account, "localhost")
+        .await
+        .unwrap();
+}
+
+#[derive(Default, Clone, Component)]
+pub struct State {}
+
+async fn handle(bot: Client, event: Event, state: State) -> anyhow::Result<()> {
+    match event {
+        Event::Chat(m) => {
+            println!("{}", m.message().to_ansi());
+        }
+        _ => {}
+    }
+
+    Ok(())
+}
