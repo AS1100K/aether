996ef0528e25201de95d7a48f4cd0861c4f662d0
Aditya Kumar <theadityakumardeveloper@gmail.com>
2024-05-22 11:16:24 +0530

    Added Event Listener for Init and Death and _ to the arguments that are not used in the current version of the bot.
    + Added help command script.
    + Removed unnecessary import.

---
+++ aether-core/src/client.rs
@@ -0,0 +1,44 @@
+use crate::config::Role;
+use crate::State;
+use azalea::protocol::packets::game::clientbound_player_combat_kill_packet::ClientboundPlayerCombatKillPacket;
+use azalea::protocol::packets::game::serverbound_client_command_packet::Action::PerformRespawn;
+use azalea::protocol::packets::game::serverbound_client_command_packet::ServerboundClientCommandPacket;
+use azalea::{Client, ClientInformation};
+use std::sync::Arc;
+
+pub async fn handle_init(client: Client, state: State) -> anyhow::Result<()> {
+    if state.config.role == Role::Pearl {
+        client
+            .set_client_information(ClientInformation {
+                view_distance: 5,
+                ..Default::default()
+            })
+            .await?;
+    }
+
+    Ok(())
+}
+
+// pub async fn handle_disconnect(
+//     client: Client,
+//     _state: State,
+//     _text: Option<FormattedText>,
+// ) -> anyhow::Result<()> {
+//     tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+//
+//     Ok(())
+// }
+
+pub async fn handle_death(
+    client: Client,
+    _state: State,
+    _death: Option<Arc<ClientboundPlayerCombatKillPacket>>,
+) -> anyhow::Result<()> {
+    let respawn_command_packet = ServerboundClientCommandPacket {
+        action: PerformRespawn,
+    };
+
+    client.write_packet(respawn_command_packet.get())?;
+
+    Ok(())
+}
