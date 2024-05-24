c17f127baa4984ff47a87d5ac549e19420254f5c
Aditya Kumar <theadityakumardeveloper@gmail.com>
2024-05-24 18:16:00 +0530

    Moved from `ClientBuilder` to `SwarmBuilder`. Added Auto-Reconnect feature. And moved up the version to v0.3.0-alpha.1

--- aether-core/src/commands/pearl/set.rs
+++ aether-core/src/commands/pearl/set.rs
@@ -1,7 +1,14 @@
 use crate::{msg, State};
 use azalea::Client;
 
-pub async fn handle_set(username: String, client: Client, _state: State, _x: i32, _y: i32, _z: i32) {
+pub async fn handle_set(
+    username: String,
+    client: Client,
+    _state: State,
+    _x: i32,
+    _y: i32,
+    _z: i32,
+) {
     msg!(
         client,
         username,
