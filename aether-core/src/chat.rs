cae962ac06eca4c32bd248cc41e2f73d3ccba88f
Aditya Kumar <theadityakumardeveloper@gmail.com>
2024-05-27 09:35:43 +0530

    fix: Able to read whisper in 2b2t.
    + Modified chat command for the bot

--- aether-core/src/chat.rs
+++ aether-core/src/chat.rs
@@ -1,12 +1,14 @@
 use crate::command::Command;
 use crate::handle_command::handle_commands;
-use crate::State;
+use crate::{msg, State};
 use azalea::chat::ChatPacket;
 use azalea::Client;
 use log::info;
+use crate::utils::parse_chat_content;
 
 pub async fn handle_chat(client: Client, chat: ChatPacket, state: State) -> anyhow::Result<()> {
-    let (username, content) = chat.split_sender_and_content();
+    println!("{:?}", parse_chat_content(&chat));
+    let (username, content, is_whisper) = parse_chat_content(&chat);
 
     if let Some(ref uname) = username {
         if uname == &state.config.username || !state.config.members.contains(uname) {
@@ -15,7 +17,7 @@ pub async fn handle_chat(client: Client, chat: ChatPacket, state: State) -> anyh
     } else if username.is_none() {
         if content.starts_with("Server") {
             for member in state.config.members {
-                client.send_command_packet(format!("msg {} {}", member, content).as_str())
+                msg!(client, member, content);
             }
         } else if content == "Connected to the server.".to_string() {
             {
@@ -28,7 +30,7 @@ pub async fn handle_chat(client: Client, chat: ChatPacket, state: State) -> anyh
         return Ok(());
     }
 
-    if *state.game_information.is_connected.lock().unwrap() && chat.is_whisper() {
+    if *state.game_information.is_connected.lock().unwrap() && is_whisper {
         let command: Command = Command::parse(content.as_str()).await;
         handle_commands(command, username.unwrap(), client, chat, state).await?;
     }
