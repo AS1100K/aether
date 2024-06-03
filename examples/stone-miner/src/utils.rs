648bbfe9e6106ecac8c1138bea216531fe1032a5
Aditya Kumar <theadityakumardeveloper@gmail.com>
2024-06-03 07:37:29 +0530

    fix: the direction of the bot is now towards the checkpoint instead of circling it or moving far from it.

--- examples/stone-miner/src/utils.rs
+++ examples/stone-miner/src/utils.rs
@@ -2,9 +2,9 @@ use azalea::Vec3;
 use log::info;
 
 pub async fn direction(end: Vec3, start: Vec3) -> anyhow::Result<f64> {
-    let dx = end.x - start.x;
-    let dz = end.z - start.z;
-    let angle = dz.atan2(dx).to_degrees();
+    let dx = (end.x - start.x).abs();
+    let dz = (end.z - start.z).abs();
+    let angle = dx.atan2(dz).to_degrees();
     info!("direction: start: ({}, {}), end: ({}, {}), angle: {}", start.x, start.z, end.x, end.z, angle);
     Ok(angle)
 }
