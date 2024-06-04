8bb11a2daff241d7d3795910a275068e0f5c323f
Aditya Kumar <theadityakumardeveloper@gmail.com>
2024-06-04 10:59:35 +0530

    Moved from macro to async function for handling bot movement.
    + Improved Logging

--- examples/stone-miner/src/utils.rs
+++ examples/stone-miner/src/utils.rs
@@ -1,14 +1,15 @@
 use azalea::Vec3;
-use log::info;
+use log::{info, trace};
 
 pub async fn direction(end: Vec3, start: Vec3) -> anyhow::Result<f64> {
     let dx = (end.x - start.x).abs();
     let dz = (end.z - start.z).abs();
     let angle = dx.atan2(dz).to_degrees();
-    info!("direction: start: ({}, {}), end: ({}, {}), angle: {}", start.x, start.z, end.x, end.z, angle);
+    trace!("direction: start: ({}, {}), end: ({}, {}), angle: {}", start.x, start.z, end.x, end.z, angle);
     Ok(angle)
 }
 
 pub async fn distance(end: Vec3, start: Vec3) -> anyhow::Result<f64> {
+    info!("distance: {}", start.distance_to(&end));
     Ok(start.distance_to(&end))
 }
