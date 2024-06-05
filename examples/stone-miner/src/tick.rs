f3c237d121df15a10ba8986f825d452c697477bd
Aditya Kumar <theadityakumardeveloper@gmail.com>
2024-06-05 13:49:08 +0530

    Added block mining capabilities
    + `config.json` now requires two more parameters `y_start` and `y_end`

--- examples/stone-miner/src/tick.rs
+++ examples/stone-miner/src/tick.rs
@@ -1,24 +1,33 @@
-use std::time::Duration;
+use crate::utils::{direction, distance, mine};
+use crate::State;
 use azalea::{Client, Vec3, WalkDirection};
 use log::{debug, info, trace};
-use crate::State;
-use crate::utils::{direction, distance};
+use std::time::Duration;
 
 async fn next_checkpoint(client: &mut Client, next_point: u8, state: &State) -> anyhow::Result<()> {
     debug!("Trying to reach next_point: {}", next_point);
     let current_position: Vec3 = client.position();
     let next_checkpoint = state.checkpoints.lock()[next_point as usize];
-    let next_checkpoint_vec: Vec3 = Vec3::new(next_checkpoint[0], next_checkpoint[1], next_checkpoint[2]);
+    let next_checkpoint_vec: Vec3 =
+        Vec3::new(next_checkpoint[0], next_checkpoint[1], next_checkpoint[2]);
 
-    let y_rot = direction(next_checkpoint_vec, current_position).await.unwrap();
+    let y_rot = direction(next_checkpoint_vec, current_position)
+        .await
+        .unwrap();
     trace!("y_rot: {}", y_rot);
 
-    client.set_direction(y_rot as f32, -71.6_f32);
+    client.set_direction(y_rot as f32, -90.0);
     client.walk(WalkDirection::Forward);
-    tokio::time::sleep(Duration::from_millis(45)).await;
+    tokio::time::sleep(Duration::from_millis(35)).await;
     client.walk(WalkDirection::None);
 
-    let dist = distance(next_checkpoint_vec, current_position).await.unwrap();
+    mine(client, state.y_start, state.y_end)
+        .await
+        .expect("Unable to mine the blocks.");
+
+    let dist = distance(next_checkpoint_vec, current_position)
+        .await
+        .unwrap();
     trace!("distance is: {}", dist);
 
     if dist <= 0.5 {
@@ -40,7 +49,7 @@ pub async fn handle_tick(mut client: Client, state: State) -> anyhow::Result<()>
             1 => 2,
             2 => 3,
             3 => 0,
-            _ => 0
+            _ => 0,
         };
     }
     next_checkpoint(&mut client, next_point, &state).await?;
--- examples/stone-miner/src/utils.rs
+++ examples/stone-miner/src/utils.rs
@@ -1,4 +1,4 @@
-use azalea::Vec3;
+use azalea::{BlockPos, BotClientExt, Client, Vec3};
 use log::{info, trace};
 
 pub async fn direction(end: Vec3, start: Vec3) -> anyhow::Result<f64> {
@@ -13,4 +13,22 @@ pub async fn distance(end: Vec3, start: Vec3) -> anyhow::Result<f64> {
     let dist = start.distance_to(&end);
     trace!("distance: {}", dist);
     Ok(dist)
+}
+
+pub async fn mine(client: &mut Client, y_start: i32, y_end: i32) -> anyhow::Result<()> {
+    let current_position = client.position();
+    let current_x: i32 = format!("{:.0}", current_position.x).parse().unwrap();
+    let current_z: i32 = format!("{:.0}", current_position.z).parse().unwrap();
+
+    // To not trigger anti-cheat the bot can mine upto 4 blocks safely
+    // for example:
+    // y_start = 6
+    // y_end = 10
+    for y in y_start..y_end {
+        let block = BlockPos::new(current_x, y, current_z);
+        trace!("Mining {:?}", block);
+        client.mine(block).await;
+    }
+
+    Ok(())
 }
