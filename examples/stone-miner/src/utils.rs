d6167ca593913e479f744d676107157cdd339e33
Aditya Kumar <theadityakumardeveloper@gmail.com>
2024-06-05 19:19:50 +0530

    fix: shifted direction function to `client.look_at`.

--- examples/stone-miner/src/utils.rs
+++ examples/stone-miner/src/utils.rs
@@ -1,14 +1,6 @@
 use azalea::{BlockPos, BotClientExt, Client, Vec3};
 use log::{info, trace};
 
-pub async fn direction(end: Vec3, start: Vec3) -> anyhow::Result<f64> {
-    let dx = (end.x - start.x).abs();
-    let dz = (end.z - start.z).abs();
-    let angle = dx.atan2(dz).to_degrees();
-    trace!("direction: start: ({}, {}), end: ({}, {}), angle: {}", start.x, start.z, end.x, end.z, angle);
-    Ok(angle)
-}
-
 pub async fn distance(end: Vec3, start: Vec3) -> anyhow::Result<f64> {
     let dist = start.distance_to(&end);
     trace!("distance: {}", dist);
