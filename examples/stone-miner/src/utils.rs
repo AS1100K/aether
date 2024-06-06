c2ddb343784177f42651d574beb84c58f9df3a42
Aditya Kumar <theadityakumardeveloper@gmail.com>
2024-06-06 07:34:23 +0530

    fix: Bot is only mining the blocks above it

--- examples/stone-miner/src/utils.rs
+++ examples/stone-miner/src/utils.rs
@@ -9,15 +9,15 @@ pub async fn distance(end: Vec3, start: Vec3) -> anyhow::Result<f64> {
 
 pub async fn mine(client: &mut Client, y_start: i32, y_end: i32) -> anyhow::Result<()> {
     let current_position = client.position();
-    let current_x: i32 = format!("{:.0}", current_position.x).parse().unwrap();
-    let current_z: i32 = format!("{:.0}", current_position.z).parse().unwrap();
+    let current_x = current_position.x.floor();
+    let current_z = current_position.z.floor();
 
     // To not trigger anti-cheat the bot can mine upto 4 blocks safely
     // for example:
     // y_start = 6
     // y_end = 10
     for y in y_start..y_end {
-        let block = BlockPos::new(current_x, y, current_z);
+        let block = BlockPos::new(current_x as i32, y, current_z as i32);
         trace!("Mining {:?}", block);
         client.mine(block).await;
     }
