ee3323340a01828b98d8c34e956dcf79faff0750
Aditya Kumar <theadityakumardeveloper@gmail.com>
2024-06-10 23:31:45 +0530

    Removed unused function

--- aether-core/src/utils.rs
+++ aether-core/src/utils.rs
@@ -19,30 +19,6 @@ pub fn distance(current: Vec3, target: Vec3) -> f64 {
     current.distance_to(&target)
 }
 
-/// Checks weather the bot can interact with the block position.
-/// This function has bugs
-pub async fn can_interact(current: Vec3, target: Vec3, world: Arc<RwLock<Instance>>) -> bool {
-    let direction = (target - current).normalize();
-
-    let mut current_pos = current;
-
-    while (current_pos - current).length() < (target - current).length() {
-        let block = world.read().get_block_state(&BlockPos::new(
-            current_pos.x as i32,
-            current_pos.y as i32,
-            current_pos.z as i32,
-        ));
-        if !block.unwrap().is_air() {
-            return false;
-        }
-
-        // Move the position along the ray
-        current_pos += direction * 0.1;
-    }
-
-    return true;
-}
-
 pub fn parse_chat_content(chat: &ChatPacket) -> (Option<String>, String, bool) {
     let (username, content) = chat.split_sender_and_content();
     // If the username is present, use split_sender_and_content
