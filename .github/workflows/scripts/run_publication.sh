240c4f120b5d3573b04ea725b6ee31c0cd6dd07b
Aditya Kumar <117935160+AS1100K@users.noreply.github.com>
2024-07-13 14:25:34 +0530

    fix: Removed unnecessary actions

--- .github/workflows/scripts/run_publication.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xeu
-
-IFS=";"
-for crate_path in $CRATE_PATHS; do
-    cd "$crate_path"
-    cargo publish --token "$CRATES_IO_TOKEN"
-    cd - || exit 1
-done
--- .github/workflows/scripts/upload_coverage_on_codecov.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -xeu
-
-grcov . --binary-path ./target/debug/ -s . -t lcov --branch --ignore-not-existing -o ./lcov.info --excl-start "#\[cfg\(test\)\]" --keep-only **/src/**/*
-bash <(curl -s https://codecov.io/bash) -f lcov.info || echo 'Codecov failed to upload'
