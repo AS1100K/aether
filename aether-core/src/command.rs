d7dc8dafe5c3643010d2640a86b17a0e4633e9dd
Aditya Kumar <117935160+AS1100K@users.noreply.github.com>
2024-08-07 18:01:09 +0530

    Migrated to ECS (#22)

--- aether-core/src/command.rs
+++ aether-core/src/command.rs
@@ -1,44 +1,26 @@
 use crate::config::{Bot, Role};
 
-pub enum Command {
+pub enum AetherCommand {
     PearlLoad,
-    PearlSet(i32, i32, i32),
     Help,
     Unknown,
-    InvalidArguments,
 }
 
-impl Command {
-    pub async fn parse(input: &str, state: &Bot) -> Command {
+impl AetherCommand {
+    pub fn parse(input: &str, state: &Bot) -> Self {
         let parts: Vec<&str> = input.split_whitespace().collect();
 
         // TODO: Port all the commands to discord and only support necessary ones via minecraft chat
         match parts.as_slice() {
             ["!pearl", "load"] => {
                 if state.role == Role::Pearl {
-                    Command::PearlLoad
+                    AetherCommand::PearlLoad
                 } else {
-                    Command::Unknown
+                    AetherCommand::Unknown
                 }
             }
-            ["!pearl", "set", x, y, z] => {
-                if state.role == Role::Pearl {
-                    let x = x.parse::<i32>();
-                    let y = y.parse::<i32>();
-                    let z = z.parse::<i32>();
-
-                    #[allow(clippy::unnecessary_unwrap)]
-                    if x.is_ok() && y.is_ok() && z.is_ok() {
-                        Command::PearlSet(x.unwrap(), y.unwrap(), z.unwrap())
-                    } else {
-                        Command::InvalidArguments
-                    }
-                } else {
-                    Command::Unknown
-                }
-            }
-            ["!help"] => Command::Help,
-            _ => Command::Unknown,
+            ["!help"] => AetherCommand::Help,
+            _ => AetherCommand::Unknown,
         }
     }
 }
--- aether-core/src/commands/mod.rs
+++ aether-core/src/commands/mod.rs
@@ -1,3 +1,21 @@
-pub mod error;
-pub mod help;
+use azalea::{
+    app::Plugin,
+    prelude::{Component, GameTick},
+};
+use pearl::load::{handle_executing_task, handle_load_peral, LoadPearl};
+
 pub mod pearl;
+
+/// This Component is present when the bot has receieved a command and is executing it
+#[derive(Component)]
+pub struct ExecutingTask;
+
+pub struct AetherCommandsPlugin;
+
+impl Plugin for AetherCommandsPlugin {
+    fn build(&self, app: &mut azalea::app::App) {
+        app.add_event::<LoadPearl>()
+            .add_systems(GameTick, handle_load_peral)
+            .add_systems(GameTick, handle_executing_task);
+    }
+}
