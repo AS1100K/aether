ef23de38569e65e6a44cf6f34c42aabd67519a44
Aditya Kumar <117935160+AS1100K@users.noreply.github.com>
2024-08-07 17:59:10 +0530

    fixed clippy warnings

--- aether-core/src/chat.rs
+++ aether-core/src/chat.rs
@@ -33,57 +33,57 @@ fn handle_chat(
 ) {
     for ChatReceivedEvent { entity, packet } in events.read() {
         for (state, in_world) in query.iter() {
-            let (username, content, is_whisper) = parse_chat_content(&packet);
+            let (username, content, is_whisper) = parse_chat_content(packet);
 
             if let Some(uname) = username {
-                if uname != state.username && config.members.contains(&uname) {
-                    if in_world.is_some() && is_whisper {
-                        // Parse the command and execute it
-                        // TODO: Notify Command received in Discord
-                        let aether_command = AetherCommand::parse(&content, &state);
-                        match aether_command {
-                            AetherCommand::PearlLoad => {
-                                load_pearl.send(LoadPearl {
-                                    entity: *entity,
-                                    username: uname,
-                                });
-                            }
-                            AetherCommand::Help => {
-                                send_chat_event.send(SendChatEvent {
+                if uname != state.username
+                    && config.members.contains(&uname)
+                    && in_world.is_some()
+                    && is_whisper
+                {
+                    // Parse the command and execute it
+                    // TODO: Notify Command received in Discord
+                    let aether_command = AetherCommand::parse(&content, state);
+                    match aether_command {
+                        AetherCommand::PearlLoad => {
+                            load_pearl.send(LoadPearl {
+                                entity: *entity,
+                                username: uname,
+                            });
+                        }
+                        AetherCommand::Help => {
+                            send_chat_event.send(SendChatEvent {
                                         entity: *entity,
                                         content: format!("/w {} Hi, I am {} a part of Project Aether. For more information see https://github.com/as1100k/aether.git or have a talk with AS1100K. Tnx!", uname, state.username),
                                     });
-                            }
-                            AetherCommand::Unknown => {
-                                send_chat_event.send(SendChatEvent {
-                                    entity: *entity,
-                                    content: format!(
-                                        "/w {} Oops! unknown command idk what to do. Ohh god!",
-                                        uname
-                                    ),
-                                });
-                            }
+                        }
+                        AetherCommand::Unknown => {
+                            send_chat_event.send(SendChatEvent {
+                                entity: *entity,
+                                content: format!(
+                                    "/w {} Oops! unknown command idk what to do. Ohh god!",
+                                    uname
+                                ),
+                            });
                         }
                     }
                 }
-            } else {
-                if content == "Connected to the server." {
-                    commands.entity(*entity).insert(InWorld);
-                    commands.entity(*entity).insert(AntiAFK {
-                        last_afk_tick: Instant::now(),
-                        has_moved: None,
-                        config: AntiAFKConfig {
-                            jump: true,
-                            sneak: false,
-                            walk: false,
-                            flip_lever: true,
-                            central_afk_location: None,
-                        },
-                    });
-                } else if content == "You have lost connection to the server" {
-                    commands.entity(*entity).remove::<InWorld>();
-                    commands.entity(*entity).remove::<AntiAFK>();
-                }
+            } else if content == "Connected to the server." {
+                commands.entity(*entity).insert(InWorld);
+                commands.entity(*entity).insert(AntiAFK {
+                    last_afk_tick: Instant::now(),
+                    has_moved: None,
+                    config: AntiAFKConfig {
+                        jump: true,
+                        sneak: false,
+                        walk: false,
+                        flip_lever: true,
+                        central_afk_location: None,
+                    },
+                });
+            } else if content == "You have lost connection to the server" {
+                commands.entity(*entity).remove::<InWorld>();
+                commands.entity(*entity).remove::<AntiAFK>();
             }
         }
     }
